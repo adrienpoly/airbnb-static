0091374e1efd4049aad47e7b9a5b6744c5173240
Adrien Poly <adrienpoly@gmail.com>
2017-02-06 18:24:15 +0100

    dynamic rating

--- config.rb
+++ config.rb
@@ -30,8 +30,12 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-["anne", "seb", "romain"].each do |name|
-  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+# ["anne", "seb", "romain"].each do |name|
+#   proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+# end
+
+data.flats.each do |owner, flat|
+  proxy "/flats/#{owner}.html", "/flats/show.html", locals: { owner: owner, flat: flat }, :ignore => true
 end
 ###
 # Helpers
